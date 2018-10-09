334d6aae3bdc220c959a7a39e708bc9822d8096b
Tara E. Sabella <tarasabella@gmail.com>
2018-10-09 02:03:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function getCart() {
 function setCart(c) {
   cart = c
 }
-
+const item = [];
 function addToCart(item) {
   var price = Math.floor(Math.random() * 100) + 1
   var objItem = {`${item}: ${price}`};
