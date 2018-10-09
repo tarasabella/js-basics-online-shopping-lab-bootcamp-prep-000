08020e637137f7a88735306f1a3d05b1f3a46464
Tara E. Sabella <tarasabella@gmail.com>
2018-10-09 01:18:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,7 +13,8 @@ function addToCart(item) {
  var price = Math.floor(Math.random() * 100) + 1;
  //price is random nondecimal number b/w 1 & 100
  var itemName = {};
-cart.push([{itemName: price}]);
+var itemPrice = {itemName: price};
+cart.push(itemPrice);
 console.log(`${item} has been added to your cart.`);
 return cart;
 }
